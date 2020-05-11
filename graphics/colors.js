941b57831902dd458ddefd168fb805ee1a5a48d9
fedesimeon <fede.simeon@gmail.com>
2020-05-11 23:35:40 +0200

    Added white color, changed pink

--- graphics/colors.js
+++ graphics/colors.js
@@ -1,7 +1,7 @@
 const COLORS = [
   { name: 'black', hex: '#000000'},
   { name: 'red', hex: '#f44336'},
-  { name: 'pink', hex: '#E91E63'},
+  { name: 'pink', hex: '#f0bdbd'},
   { name: 'purple', hex: '#9C27B0'},
   { name: 'deeppurple', hex: '#673AB7'},
   { name: 'indigo', hex: '#3F51B5'},
@@ -14,9 +14,9 @@ const COLORS = [
   { name: 'yellow', hex: '#FFEB3B'},
   { name: 'amber', hex: '#FFC107'},
   { name: 'orange', hex: '#FF9800'},
-  { name: 'deeporange', hex: '#FF5722'},
   { name: 'brown', hex: '#795548'},
-  { name: 'grey', hex: '#9E9E9E'}
+  { name: 'grey', hex: '#9E9E9E'},
+  { name: 'white', hex: '#FFFFFF'}
 ];
 
 //Listener for each button associated to the colors
